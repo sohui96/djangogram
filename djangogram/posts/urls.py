bb3544b24f929eaf9079575e8edd431523bbac03
김소희 <sohuig@sookmyung.ac.kr>
2021-06-24 15:22:51 +0900

    add index-nav-style

--- djangogram/posts/urls.py
+++ djangogram/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pages/', views.home, name='home'),
 ]
--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+def home(request):
+    return render(request, 'pages/home.html')    
